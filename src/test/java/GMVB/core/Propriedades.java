02516a91b1c24e32d08a07ef35a95a3fcf59064c
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-17 18:21:58 -0300

    AT

---
+++ src/test/java/GMVB/core/Propriedades.java
@@ -0,0 +1,15 @@
+package GMVB.core;
+
+import GMVB.core.Propriedades.Browsers;
+
+public class Propriedades {
+
+	public static boolean FECHAR_BROWSER = true;
+
+	public static Browsers browser = Browsers.CHROME;
+
+	public enum Browsers {
+		CHROME
+	}
+
+}
